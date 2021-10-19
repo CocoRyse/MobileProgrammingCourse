913f34b8f914983ff39e5a3eab120de83cd8d2ac
Виктор Решетов <vityareshetov@yandex.com>
2021-10-19 22:19:06 +0300

    Переписал загрузку данных

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,34 +1,43 @@
-import React, { Fragment, useState } from "react";
-import { View, Text, StyleSheet, Button, Image, FlatList } from "react-native";
-
-let res;
-
-const getRes = async () => {
-  try {
-    res = (
-      await (await fetch("http://swapi.dev/api/planets/")).json()
-    ).results.map((item, index) => {
-      return { ...item, key: index };
-    });
-    console.log(res);
-  } catch {
-    console.log("не повезло");
-    return ["Не повезло, дружище"];
-  }
-};
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const MainScreen = () => {
-  if (!res) {
-    getRes();
-  }
+  const [planets, setPlanets] = useState();
+
+  const fetchPlanets = async () => {
+    try {
+      const response = await fetch("http://swapi.dev/api/planets/");
+      const planetsJson = await response.json();
+      const planets = planetsJson.results.map((planet, index) => {
+        return {
+          key: index.toString(), // необходимо для того, чтобы не было проблем с открисовкой списка
+          ...planet,
+        };
+      });
+      setPlanets(planets);
+    } catch {
+      // Хорошо бы хендлить ошибку по-человечески, но это потом
+      console.error("Не повезло, не повезло");
+    }
+  };
+
+  // Хук useEffect позволяет управлять
+  // различными сопутствующими действиями в функциональном компоненте —
+  // то, что называется побочными эффектами
+  // В нашем случае это вызов метода SWAPI
+  // https://ru.reactjs.org/docs/hooks-reference.html#useeffect
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
 
   const renderListItem = ({ item }) => {
-    return <Text style={styles.text}>{item.json()}</Text>;
+    return <Text style={styles.text}>{item.name}</Text>;
   };
 
   return (
-    <View style={styles.container} accessibilityLabel={res?.length}>
-      <FlatList data={res} renderItem={renderListItem}></FlatList>
+    <View style={styles.container}>
+      <Text style={styles.header}>swapi.dev/api/planets/</Text>
+      <FlatList data={planets} renderItem={renderListItem}></FlatList>
     </View>
   );
 };
@@ -41,13 +50,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#faf",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 50,
   },
   image: {
     width: 300,
     height: 300,
     marginBottom: 20,
   },
+  header: {
+    fontSize: 30,
+    marginBottom: 15,
+  },
   text: {
-    backgroundColor: "#fff",
+    fontSize: 25,
+    margin: 15,
   },
 });
