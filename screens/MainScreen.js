1e7838f6f0f8b6d679e7a54152326b5871bb3005
Виктор Решетов <vityareshetov@yandex.com>
2021-10-26 19:26:22 +0300

    Список со всеми персонажами Start Wars

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,33 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import { getAllPeople } from "../services/swapi";
 
 const MainScreen = () => {
-  const [planets, setPlanets] = useState();
-
-  const fetchPlanets = async () => {
-    try {
-      const response = await fetch("http://swapi.dev/api/planets/");
-      const planetsJson = await response.json();
-      const planets = planetsJson.results.map((planet, index) => {
+  const [people, setPeople] = useState();
+  useEffect(() => {
+    const handlePeopleUpdate = (peoples) => {
+      const people = peoples.map((item, index) => {
         return {
-          key: index.toString(), // необходимо для того, чтобы не было проблем с открисовкой списка
-          ...planet,
+          key: index.toString(),
+          ...item,
         };
       });
-      setPlanets(planets);
-    } catch {
-      // Хорошо бы хендлить ошибку по-человечески, но это потом
-      console.error("Не повезло, не повезло");
-    }
-  };
+      setPeople(people);
+    };
 
-  // Хук useEffect позволяет управлять
-  // различными сопутствующими действиями в функциональном компоненте —
-  // то, что называется побочными эффектами
-  // В нашем случае это вызов метода SWAPI
-  // https://ru.reactjs.org/docs/hooks-reference.html#useeffect
-  useEffect(() => {
-    fetchPlanets();
+    getAllPeople(handlePeopleUpdate);
   }, []);
 
   const renderListItem = ({ item }) => {
@@ -36,8 +24,8 @@ const MainScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>swapi.dev/api/planets/</Text>
-      <FlatList data={planets} renderItem={renderListItem}></FlatList>
+      <Text style={styles.header}>Список персонажей Star Wars</Text>
+      <FlatList data={people} renderItem={renderListItem}></FlatList>
     </View>
   );
 };
