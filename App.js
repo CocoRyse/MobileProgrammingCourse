fe837e1c36b3180f39ff63809044ee63fa8148e5
Виктор Решетов <vityareshetov@yandex.com>
2021-11-02 17:05:42 +0300

    Добавил шаблон в приложении

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import MainScreen from "./screens/MainScreen";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <MainScreen></MainScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={MainScreen}
+          options={{ title: "Welcome" }}
+        />
+        {/* <Stack.Screen name="Species" component={ProfileScreen} />
+        <Stack.Screen name="Planets" component={ProfileScreen} />
+        <Stack.Screen name="People" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
